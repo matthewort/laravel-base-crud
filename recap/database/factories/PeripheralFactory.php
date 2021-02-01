a3706508e4656d365c45f00f7d317e4f2573d015
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2021-02-01 22:31:51 +0100

    Migrations

---
+++ recap/database/factories/PeripheralFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Peripheral;
+use Faker\Generator as Faker;
+
+$factory->define(Peripheral::class, function (Faker $faker) {
+    return [
+        'name' => $faker -> word(),
+        'model' => $faker -> ean13(),
+        'price' => rand(),
+        'consumption' => rand(20, 50)
+    ];
+});
---
+++ recap/database/migrations/2021_02_01_180108_create_peripherals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeripheralsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peripherals', function (Blueprint $table) {
+            $table->id();
+
+            $table -> string('name', 50);
+            $table -> string('model', 100) -> unique();
+            $table -> integer('price');
+            $table -> integer('consumption');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peripherals');
+    }
+}
